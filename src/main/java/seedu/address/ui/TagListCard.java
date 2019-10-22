b3ec328ac1d234b250c3166a3b6b213589d26b91
chowyiwai <yiwai.chow99@gmail.com>
2019-10-22 20:37:38 +0800

    Update GUI to have different colours for default and user tags

--- src/main/java/seedu/address/ui/TagListCard.java
+++ src/main/java/seedu/address/ui/TagListCard.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import seedu.address.model.Color;
 import seedu.address.model.tag.Tag;
 
 /**
