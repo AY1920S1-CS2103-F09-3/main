3056ae774aa4dbaba3ccfd961f3ad61e977089d7
Andy Lam <andyylam1997@gmail.com>
2019-10-23 22:50:08 +0800

    Implementing more tests for Module

--- src/test/java/seedu/address/model/module/ModuleTest.java
+++ src/test/java/seedu/address/model/module/ModuleTest.java
@@ -2,37 +2,58 @@ package seedu.address.model.module;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.TypicalModulesInfo.CS1101S;
-import static seedu.address.testutil.TypicalModulesInfo.CS2040S;
-
-import java.util.ArrayList;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_ST2334;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_ST2334;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_ST2334;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_ST2334;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.TypicalModule.CS3244;
+import static seedu.address.testutil.TypicalModule.ST2334;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.Color;
-import seedu.address.model.tag.Tag;
+import seedu.address.testutil.ModuleBuilder;
 
 /**
  * A test class for {@code Module}.
  */
 public class ModuleTest {
-
     @Test
     public void equals() {
-        // same module -> true
-        assertTrue(CS1101S.equals(CS1101S));
+        // same values -> returns true
+        Module CS3244_COPY = new ModuleBuilder(CS3244).build();
+        assertTrue(CS3244.equals(CS3244_COPY));
 
-        // different module -> false
-        assertFalse(CS1101S.equals(CS2040S));
+        // same object -> returns true
+        assertTrue(CS3244.equals(CS3244));
 
-        // skeletal modules
-        Module skeletalCS1101S = new Module(new ModuleCode("CS1101S"), Color.RED, new ArrayList<Tag>());
-        Module skeletalCS2040S = new Module(new ModuleCode("CS2040S"), Color.RED, new ArrayList<Tag>());
-        assertFalse(skeletalCS1101S.equals(CS1101S));
-        assertFalse(CS1101S.equals(skeletalCS1101S));
-        assertFalse(skeletalCS2040S.equals(CS2040S));
-        assertFalse(CS2040S.equals(skeletalCS2040S));
+        // null -> returns false
+        assertFalse(CS3244.equals(null));
 
+        // different type -> returns false
+        assertFalse(CS3244.equals(5));
+
+        // different person -> returns false
+        assertFalse(CS3244.equals(ST2334));
+
+        // different name -> returns false
+        Module editedCS3244 = new ModuleBuilder(CS3244).withName(VALID_NAME_ST2334).build();
+        assertFalse(CS3244.equals(editedCS3244));
+
+        // different phone -> returns false
+        editedCS3244 = new ModuleBuilder(CS3244).withPhone(VALID_PHONE_ST2334).build();
+        assertFalse(CS3244.equals(editedCS3244));
+
+        // different email -> returns false
+        editedCS3244 = new ModuleBuilder(CS3244).withEmail(VALID_EMAIL_ST2334).build();
+        assertFalse(CS3244.equals(editedCS3244));
+
+        // different address -> returns false
+        editedCS3244 = new ModuleBuilder(CS3244).withAddress(VALID_ADDRESS_ST2334).build();
+        assertFalse(CS3244.equals(editedCS3244));
+
+        // different tags -> returns false
+        editedCS3244 = new ModuleBuilder(CS3244).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(CS3244.equals(editedCS3244));
     }
-
 }
